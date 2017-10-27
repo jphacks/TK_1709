98ee7a761eff62973fa16554a7e757cf2df44b0e
中村龍矢 <nrryuya1@nrryuya.local>
2017-10-28 08:38:47 +0900

    urlsの設定

--- api/api/urls.py
+++ api/api/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from exhibit.urls import router as exhibit_router
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^exhibit/', include(exhibit_router.urls)),
+    url(r'^returning/', include('returning.urls')),
+    url(r'^split/', include('split.urls')),
+    url(r'^rent/', include('rent.urls'))
 ]
